3157e8168017a4edc0fd8698d2b9251a4beefaa4
jk872001 <72179040+jk872001@users.noreply.github.com>
2022-09-20 11:23:30 +0530

    chat fetched successfully

--- server/index.js
+++ server/index.js
@@ -1,31 +1,24 @@
 require("dotenv").config();
-const express=require("express");
-const app=express();
-app.use(express.json())
-const chats=require("./data/data")
+const express = require("express");
+var cors = require('cors')
 
 
-app.get("/api/chat",(req,res)=>
-{
-    res.json(chats)
-})
+const app = express();
+app.use(express.json());
+app.use(cors()) // Use this after the variable declaration
+const chats = require("./data/data");
 
-app.get("/api/chat/:id",(req,res)=>
-{
-    const singleUser= chats.find((e)=>e._id===req.params.id)
-    console.log(singleUser)
-    res.send(singleUser)
-})
+app.get("/api/chats", (req, res) => {
+  res.json(chats);
+});
 
+app.get("/api/chat/:id", (req, res) => {
+  const singleUser = chats.find((e) => e._id === req.params.id);
+  // console.log(singleUser)
+  res.send(singleUser);
+});
 
-
-
-
-
-
-
-const {PORT}=process.env
-app.listen(PORT,()=>
-{
-    console.log(`Server is running on ${PORT}`);
-})
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
